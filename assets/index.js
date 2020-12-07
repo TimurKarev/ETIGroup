e1397a2a276f197edb2b326557dc4fb1c7513377
TimurKarev <timurkarev@mail.ru>
2020-12-07 18:01:08 +0300

    vue integrate

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import hello from "/assets/vue/components/hello.vue";
+
+const app = new Vue({
+  el: "#app",
+  delimiters: ['[[',']]'],
+  components:{ hello
+      },
+  data: {
+    d: 'jfdslkjfdlsk'
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './assets/index.js',  // path to our input file
+  output: {
+    filename: 'index-bundle.js',  // output bundle file name
+    path: path.resolve(__dirname, './static'),  // path to our Django static directory
+  },
+  resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      // this will apply to both plain `.js` files
+      // AND `<script>` blocks in `.vue` files
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      // this will apply to both plain `.css` files
+      // AND `<style>` blocks in `.vue` files
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin for the magic
+    new VueLoaderPlugin()
+  ]
+}
