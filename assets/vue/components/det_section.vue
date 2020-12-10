3122b68fba7375fd9644b58e3d0eec6b3a562205
TimurKarev <timurkarev@mail.ru>
2020-12-10 14:35:13 +0300

    section roll ready

--- assets/vue/components/det_section.vue
+++ assets/vue/components/det_section.vue
@@ -1,21 +1,27 @@
 <template>
-<dev>
-  <div class="row">
-    <div class="header" v-bind:class="[is_plus ? 'col-7' : 'col-8']">
-      <a class="roll" href="" @click.prevent="rollUp">{{ rollSymbol}}</a>
-      {{ name }}
+<div>
+  <div class="row" v-bind:class="[is_plus ? 'close-header' : 'open-header']">
+    <div v-bind:class="[is_plus ? 'col-7' : 'col-8']"
+         :v-bind:class="[is_plus ? 'col-md-7' : 'col-md-8']">
+      <a class="header" href="" @click.prevent="rollUp">{{ rollSymbol}} {{ name }}</a>
     </div>
-    <div v-if="is_plus" class="col-1" v-bind:style="{'background-color': 'red'}">
+    <div v-if="is_plus"
+         class="col-1 col-md-1 section_box"
+         v-bind:style="{'background-color': sectionColor}">
     </div>
   </div>
   <div v-if="!is_plus">
-    <det_point v-for="point in section.points" :point="point"></det_point>
+    <det_point v-for="point in section.points" :point="point" ></det_point>
   </div>
-</dev>
+
+</div>
 </template>
 
 <script>
 import det_point from "./det_point";
+
+const completedColor = 'lime'
+const inProgressColor = 'royalblue'
 
 export default {
   name: "det_section",
@@ -29,13 +35,20 @@ export default {
   data: function (){
     return {
       'name': this.section.name,
-      'is_plus': false,
+      'is_plus': true,
     }
   },
   computed: {
     rollSymbol(){
       if (this.is_plus) return '+';
       else return '-';
+    },
+    sectionColor() {
+      let retColor = inProgressColor;
+      this.section.points.forEach(point =>{
+        if (point.value === 'Пройдены') retColor = completedColor;
+      });
+      return retColor;
     }
   },
   methods: {
@@ -47,13 +60,19 @@ export default {
 </script>
 
 <style scoped>
+
   .header {
     font-size: x-large;
-    font-weight: bolder;
-    margin-top: 10px;
-  }
-  .roll {
-    font-size: xx-large;
+    font-weight: 500;
+    line-height: 90%;
     color: black
+  }
+  .open-header{
+    padding-bottom: .4%;
+    padding-top: 1%;
+  }
+  .close-header{
+    padding-bottom: 0;
+    padding-top: .15%;
   }
 </style>
