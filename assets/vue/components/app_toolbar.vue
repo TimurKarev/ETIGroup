5f4def576f9c9b0e716461e75442fa40efb30f29
TimurKarev <timurkarev@mail.ru>
2020-12-18 16:11:50 +0300

    el layout try

--- assets/vue/components/app_toolbar.vue
+++ assets/vue/components/app_toolbar.vue
@@ -1,16 +1,50 @@
 <template>
+  <div>
   <el-menu
-      :default-active="activeIndex"
       class="el-menu-demo"
       mode="horizontal"
-      @select="handleSelect"
   >
-    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-row type="flex">
+      <el-col :span="12">
+        <el-row type="flex">
+          <el-col :span="6">
+            <el-menu-item>
+            <i class="el-icon-s-unfold"></i>
+          </el-menu-item>
+          </el-col>
+          <el-col :span="6">
+            <el-menu-item>
+            <i class="el-icon-s-home"></i>
+          </el-menu-item>
+          </el-col>
+        </el-row>
+      </el-col>
+
+      <el-col :span="12">
+        <el-row type="flex" justify="end">
+          <el-col :span="18" >
+            <el-menu-item index="1" style="{justify-content: end}">Незнакомец</el-menu-item>
+          </el-col>
+          <el-col :span="6">
+           <el-menu-item>
+        <i class="el-icon-s-custom"></i>
+      </el-menu-item>
+          </el-col>
+        </el-row>
+  </el-col>
+    </el-row>
+
   </el-menu>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-
+ .el-menu-demo {
+   height: 10%!important;
+ }
+ .el-icon-s-unfold {
+   height: 50%;
+ }
 </style>
 
 <script>
