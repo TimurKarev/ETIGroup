1e8c7c42b52adb71d9a9a8782fd7a7dc81722cbe
TimurKarev <timurkarev@mail.ru>
2020-12-18 14:41:22 +0300

    UI Element theme

--- assets/vue/components/app_toolbar.vue
+++ assets/vue/components/app_toolbar.vue
@@ -1,5 +1,12 @@
 <template>
-  <el-button type="primary">Primary</el-button>
+  <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+  >
+    <el-menu-item index="1">Processing Center</el-menu-item>
+  </el-menu>
 </template>
 
 <style lang="scss" scoped>
