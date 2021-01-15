d95776b327151a076ff8a3c25e4640fbd310812d
TimurKarev <timurkarev@mail.ru>
2021-01-15 15:13:40 +0300

    postgress db

--- ETIGroup_project/settings.py
+++ ETIGroup_project/settings.py
@@ -83,8 +83,13 @@ WSGI_APPLICATION = 'ETIGroup_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'checklist_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
