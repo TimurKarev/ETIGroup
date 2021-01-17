e5d00599c39a2d0ce7159bbc8cf68adfeca8b669
TimurKarev <timurkarev@mail.ru>
2021-01-17 22:48:48 +0300

    settings change for heroku

--- ETIGroup_project/settings.py
+++ ETIGroup_project/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,7 +81,6 @@ WSGI_APPLICATION = 'ETIGroup_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-db_from_env = dj_database_url.config(conn_max_age=600)
 
 DATABASES = {
     'default': {
@@ -94,8 +92,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-DATABASES['default'].update(db_from_env)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
