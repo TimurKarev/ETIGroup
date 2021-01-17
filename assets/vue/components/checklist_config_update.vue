bab9ba683376d80ff128c268a716b430cf2cb8fb
TimurKarev <timurkarev@mail.ru>
2021-01-18 00:35:18 +0300

    bundle push

--- assets/vue/components/checklist_config_update.vue
+++ assets/vue/components/checklist_config_update.vue
@@ -69,7 +69,7 @@ export default {
       })
       .then(response => {
         console.log(response.data);
-        location.replace("/" + response.data.message);
+        location.replace(response.data.message);
       });
     }
   },
